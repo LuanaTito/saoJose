0020aeafbbf73963311063d0451aa4d629708e78
Your Name <you@example.com>
2024-03-03 02:16:57 -0300

    midiaQuery2

---
+++ js/interacoes.js
@@ -0,0 +1,10 @@
+//Hamburger
+function clickMenu(){
+    
+    if(itens.style.display == 'block'){
+        itens.style.display = 'none'
+    }else{
+        itens.style.display = 'block'
+    }
+
+}
